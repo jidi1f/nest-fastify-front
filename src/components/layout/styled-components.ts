443c4d9ab963ee5724c7f13a20b3084e133ba6a1
Chernysh Danil <45766812+jidi1f@users.noreply.github.com>
2024-04-09 16:01:32 +0300

    feat: initial layout (#2)

---
+++ src/components/layout/styled-components.ts
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+import { Box } from '@mui/material';
+
+export const AppContainer = styled(Box)`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Main = styled(Box)`
+  height: 100%;
+  min-height: 100vh;
+  width: 100%;
+  padding: 100px 50px 50px 50px;
+  display: flex;
+  flex-direction: row;
+  background-color: #4d4c7d;
+`;
